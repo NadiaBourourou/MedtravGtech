2e98e8fed104a84e1c618a05230fee9556a61b0c
asmabouktif <asma.bouktif@esprit.tn>
2015-11-20 17:21:45 +0100

    Hello! it's me Infrastructure

---
+++ Data/Infrastructure/DatabaseFactory.cs
@@ -0,0 +1,31 @@
+﻿using Data;
+using Data.Interfaces;
+using Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Infrastructure
+{
+    public class DatabaseFactory : Disposable, IDatabaseFactory
+    {
+        private medtravdbContext dataContext;
+        public medtravdbContext DataContext
+        {
+            get { return dataContext; }
+        }
+
+        public DatabaseFactory()
+        {
+            dataContext = new medtravdbContext();
+        }
+        protected override void DisposeCore()
+        {
+            if (DataContext != null)
+                DataContext.Dispose();
+        }
+
+    }
+}
---
+++ Data/Infrastructure/UnitOfWork.cs
@@ -0,0 +1,52 @@
+﻿using Data;
+using Data.Interfaces;
+using Data.Models;
+using Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace  Data.Infrastructure
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private medtravdbContext dataContext;
+        protected medtravdbContext DataContext
+        {
+            get
+            {
+                return dataContext = dbFactory.DataContext;
+            }
+        }
+
+        IDatabaseFactory dbFactory;
+        public UnitOfWork(IDatabaseFactory dbFactory)
+        {
+            this.dbFactory = dbFactory;
+        }
+        public void Commit()
+        {
+            DataContext.SaveChanges();
+        }
+
+
+     
+
+        private IHotelRepository hotelRepository;
+        public IHotelRepository HotelRepository
+        {
+            get { return hotelRepository = new HotelRepository(dbFactory); ; }
+        }
+
+         
+
+        
+        public void Dispose()
+        {
+            DataContext.Dispose();
+        }
+        
+    }
+}
